ff416727adda805d5d81ea29fb4fb0ad0685178a
Aaron Oeder <aoeder94@gmail.com>
2023-12-15 08:39:04 -0600

    Add day 15

--- src/main/java/me/oeder/aoc/AdventRunner.java
+++ src/main/java/me/oeder/aoc/AdventRunner.java
@@ -2,7 +2,7 @@ package me.oeder.aoc;
 
 public class AdventRunner {
 	private static final int YEAR = 2023;
-	private static final int DAY = 14;
+	private static final int DAY = 15;
 	
 	private static AdventDay getAdventDay() throws Exception {
 		return (AdventDay)Class.forName(String.format("me.oeder.aoc.days%d.Day%s", YEAR, String.format("%02d", DAY)))
