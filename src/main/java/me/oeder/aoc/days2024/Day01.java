834ae7101661a5621f3f3bb38a55ac1955dc7daa
Aaron Oeder <aaron@aaronoeder.com>
2024-11-30 23:38:34 -0600

    Add 2024 Day 1

--- src/main/java/me/oeder/aoc/days2024/Day01.java
+++ src/main/java/me/oeder/aoc/days2024/Day01.java
@@ -1,5 +1,7 @@
 package me.oeder.aoc.days2024;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Day01 extends AdventDay2024 {
@@ -9,12 +11,38 @@ public class Day01 extends AdventDay2024 {
     }
 
     @Override
-    public Object solvePart1(List<String> lines) {
-        return null;
-    }
+    public Object getAnswer(List<String> lines, Part part) {
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
 
-    @Override
-    public Object solvePart2(List<String> lines) {
-        return null;
+        for (String line : lines) {
+            String[] parts = line.split("   ");
+            left.add(Integer.parseInt(parts[0]));
+            right.add(Integer.parseInt(parts[1]));
+        }
+
+        Collections.sort(left);
+        Collections.sort(right);
+
+        if (part == Part.ONE) {
+            int diff = 0;
+            for (int i = 0; i < left.size(); i++) {
+                diff += Math.abs(right.get(i) - left.get(i));
+            }
+            return diff;
+        }
+
+        int similarity = 0;
+        for (int i = 0; i < left.size(); i++) {
+            int num = left.get(i);
+            int times = 0;
+            for (int j = 0; j < right.size(); j++) {
+                if (num == right.get(j)) {
+                    times++;
+                }
+            }
+            similarity += num * times;
+        }
+        return similarity;
     }
 }
