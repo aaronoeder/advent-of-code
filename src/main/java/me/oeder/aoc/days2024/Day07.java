0204a15e47ca521fb4c06e3f82041257400144c2
Aaron Oeder <aaron@aaronoeder.com>
2024-12-06 23:33:32 -0600

    Add 2024 Day 7

---
+++ src/main/java/me/oeder/aoc/days2024/Day07.java
@@ -0,0 +1,42 @@
+package me.oeder.aoc.days2024;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day07 extends AdventDay2024 {
+
+    public Day07() {
+        super(7);
+    }
+
+    @Override
+    public Object getAnswer(List<String> lines, Part part) {
+        long sum = 0;
+        for (String line : lines) {
+            String[] parts = line.split(": ");
+            long val = Long.parseLong(parts[0]);
+            List<Long> nums = Arrays.asList(parts[1].split(" ")).stream().map(s -> Long.parseLong(s)).collect(Collectors.toList());
+
+            List<Long> possibleResults = new ArrayList<>();
+            populatePossibleResults(possibleResults, nums, 0, 0, part);
+            if (possibleResults.contains(val)) {
+                sum += val;
+            }
+        }
+        return sum;
+    }
+
+    private void populatePossibleResults(List<Long> possibleResults, List<Long> nums, long result, int index, Part part) {
+        if (index == nums.size()) {
+            possibleResults.add(result);
+        } else {
+            populatePossibleResults(possibleResults, nums, result + nums.get(index), index + 1, part);
+            populatePossibleResults(possibleResults, nums, result * nums.get(index), index + 1, part);
+            if (part == Part.TWO) {
+                populatePossibleResults(possibleResults, nums, Long.parseLong("" + result + nums.get(index)), index + 1, part);
+            }
+        }
+    }
+}
