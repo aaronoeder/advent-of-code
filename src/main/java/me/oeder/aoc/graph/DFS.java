2ed915e58aafc0898c0b953488f3099874860c60
Aaron Oeder <aaron@aaronoeder.com>
2024-12-12 00:32:49 -0600

    Add 2024 Day 12

--- src/main/java/me/oeder/aoc/graph/DFS.java
+++ src/main/java/me/oeder/aoc/graph/DFS.java
@@ -30,4 +30,24 @@ public final class DFS {
 			traverseGraph(grid, new HashSet<>(visited), uniquePaths, neighbor, end, includeDiagonalNeighbors, isValidNeighbor);
 		}
 	}
+
+	public static <T> void traverseGraph(T[][] grid, Set<Coord> visited, Coord cur, Coord end, boolean includeDiagonalNeighbors, BiPredicate<Coord, Coord> isValidNeighbor) {
+		visited.add(cur);
+		if (cur.equals(end)) {
+			return;
+		}
+
+		for (Coord neighbor : cur.getNeighbors(includeDiagonalNeighbors)) {
+			if (neighbor.getRow() < 0 || neighbor.getRow() >= grid.length || neighbor.getCol() < 0 || neighbor.getCol() >= grid[0].length) {
+				continue;
+			}
+			if (visited.contains(neighbor)) {
+				continue;
+			}
+			if (!isValidNeighbor.test(cur, neighbor)) {
+				continue;
+			}
+			traverseGraph(grid, visited, neighbor, end, includeDiagonalNeighbors, isValidNeighbor);
+		}
+	}
 }
