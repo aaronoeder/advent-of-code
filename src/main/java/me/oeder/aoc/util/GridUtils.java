4f86b27b3a47263eb4215472666915bcf89f753d
Aaron Oeder <aaron@aaronoeder.com>
2024-12-15 16:39:00 -0600

    Add 2024 Day 15

---
+++ src/main/java/me/oeder/aoc/util/GridUtils.java
@@ -0,0 +1,21 @@
+package me.oeder.aoc.util;
+
+import me.oeder.aoc.common.Coord;
+
+import java.util.function.Predicate;
+
+public final class GridUtils {
+    private GridUtils() {}
+
+    public static Coord getCoordInGridHaving(Character[][] grid, Predicate<Character> evaluator) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                char ch = grid[i][j];
+                if (evaluator.test(ch)) {
+                    return new Coord(i, j);
+                }
+            }
+        }
+        return null;
+    }
+}
