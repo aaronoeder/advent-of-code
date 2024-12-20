8ff38d04f50f4f7965f92c85d91b85adaf6e7d02
Aaron Oeder <aaron@aaronoeder.com>
2024-12-20 00:50:12 -0600

    Add 2024 Day 20

--- src/main/java/me/oeder/aoc/common/Coord.java
+++ src/main/java/me/oeder/aoc/common/Coord.java
@@ -27,4 +27,25 @@ public class Coord {
 		}
 		return neighbors;
 	}
+
+	public List<Coord> getNeighbors(Character[][] grid, boolean includeDiagonal) {
+		List<Coord> neighbors = new ArrayList<>();
+		for (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
+				if (i == 0 && j == 0) {
+					continue;
+				}
+				if (!includeDiagonal && (i * j != 0)) {
+					continue;
+				}
+				int newRow = row + i;
+				int newCol = col + j;
+				if (newRow < 0 || newRow >= grid.length || newCol < 0 || newCol >= grid.length) {
+					continue;
+				}
+				neighbors.add(new Coord(newRow, newCol));
+			}
+		}
+		return neighbors;
+	}
 }
