44b0503ea32e0ce883a981cfe6bff1d82fe33e64
Aaron Oeder <aaron@aaronoeder.com>
2025-11-30 23:18:05 -0600

    Add 2025 Day 1

--- src/main/java/com/aaronoeder/adventofcode/days2025/Day01.java
+++ src/main/java/com/aaronoeder/adventofcode/days2025/Day01.java
@@ -8,6 +8,25 @@ public class Day01 extends AdventDay {
 
     @Override
     public Object getAnswer(List<String> lines, Part part) {
-        return lines.getFirst();
+        int total = 0;
+        int pos = 50;
+        for (String line : lines) {
+            String dir = line.substring(0, 1);
+            int val = Integer.parseInt(line.substring(1));
+            for (int i = 0; i < val; i++) {
+                if (dir.equals("L")) {
+                    pos = (pos == 0 ? 99 : pos - 1);
+                } else {
+                    pos = (pos == 99 ? 0 : pos + 1);
+                }
+                if (pos == 0 && part == Part.TWO) {
+                    total++;
+                }
+            }
+            if (pos == 0 && part == Part.ONE) {
+                total++;
+            }
+        }
+        return total;
     }
 }
